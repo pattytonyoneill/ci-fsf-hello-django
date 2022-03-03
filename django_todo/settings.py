f95ff3554a84d2571781c4a64442ea93dc3cbf0e
pattytonyoneill <pattytonyoneill@gmail.com>
2022-03-03 14:17:35 +0000

    Add missing line of code

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-9g7()s@p+#)8)z4m)=q-!yg95ois-i+bkr&fv*^j9n&^-@np%z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ckz8780-django-todo-app14.herokuapp.com']
 
 
 # Application definition
