d6606a8385e721db8893575dbc2781aaf05186f2
pattytonyoneill <pattytonyoneill@gmail.com>
2022-03-15 15:31:39 +0000

    Add allowed_hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://git.heroku.com/django-todo-app14.git']
+ALLOWED_HOSTS = ['django-todo-app14.herokuapp.com']
 
 
 # Application definition
