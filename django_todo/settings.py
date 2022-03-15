100780665fac98bdf914e14de486a8fe27894235
pattytonyoneill <pattytonyoneill@gmail.com>
2022-03-15 13:55:35 +0000

    Edit of settings,py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-todo-app14.herokuapp.com/']
+ALLOWED_HOSTS = ['https://git.heroku.com/django-todo-app14.git']
 
 
 # Application definition
